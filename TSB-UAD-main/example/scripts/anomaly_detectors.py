8cbf329fc0670a1b1eab9a5d5182a5bbb911b8e8
Leonardo Falanga <info@leonardofalanga.it>
2025-07-10 15:20:07 +0200

    Report

---
+++ TSB-UAD-main/example/scripts/anomaly_detectors.py
@@ -0,0 +1,49 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+from TSB_UAD.models.distance import Fourier
+from TSB_UAD.models.feature import Window
+from TSB_UAD.utils.slidingWindows import find_length,plotFig, printResult
+from sklearn.preprocessing import MinMaxScaler
+
+from TSB_UAD.models.iforest import IForest
+
+# Data Preprocessing
+
+filepath = '../../data/benchmark/ECG/MBA_ECG805_data.out'
+df = pd.read_csv(filepath, header=None).dropna().to_numpy()
+
+name = filepath.split('/')[-1]
+max_length = 10000
+
+data = df[:max_length,0].astype(float)
+label = df[:max_length,1]
+    
+slidingWindow = find_length(data)
+X_data = Window(window = slidingWindow).convert(data).to_numpy()
+
+
+data_train = data[:int(0.1*len(data))]
+data_test = data
+
+X_train = Window(window = slidingWindow).convert(data_train).to_numpy()
+X_test = Window(window = slidingWindow).convert(data_test).to_numpy()
+
+
+
+modelName='IForest'
+clf = IForest(n_jobs=1)
+x = X_data
+clf.fit(x)
+score = clf.decision_scores_
+
+# Post processing
+score = MinMaxScaler(feature_range=(0,1)).fit_transform(score.reshape(-1,1)).ravel()
+score = np.array([score[0]]*math.ceil((slidingWindow-1)/2) + list(score) + [score[-1]]*((slidingWindow-1)//2))
+
+# save result as figure
+plotFig(data, label, score, slidingWindow, fileName=name, modelName=modelName)
+plt.savefig(modelName+'.png')
+plt.close()
+
